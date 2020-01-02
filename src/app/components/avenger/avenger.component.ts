9db1a49663ec007adb281977230832528bbd21ba
merimari0510 <merimari051094@gmail.com>
2020-01-02 16:12:43 +0100

    Crear un Caroussel con las fotos de los avengers, añadir el titulo centrado, con la descripcion del Avenger y una foto, una citacion con el video

--- src/app/components/avenger/avenger.component.ts
+++ src/app/components/avenger/avenger.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AvengersService } from '../../services/avengers.service'
 import { ActivatedRoute } from '@angular/router'
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-avenger',
@@ -10,13 +11,27 @@ import { ActivatedRoute } from '@angular/router'
 export class AvengerComponent {
 
   avenger:any = {};
+  safeUrl:any;
 
   constructor(private activatedRoute: ActivatedRoute,
-              private _avengersService:AvengersService
+              private _avengersService:AvengersService,
+              private _sanitizer: DomSanitizer
   ) {
     this.activatedRoute.params.subscribe( params => {
       this.avenger = this._avengersService.getAvenger(params['element']);
     })
+  }
+
+  getVideoIframe() {
+    let video = this.avenger.video;
+    this.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl(video);
+    return this.safeUrl;
+  }
+
+  getCiteVideoIframe() {
+    let video = this.avenger.cite_video;
+    this.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl(video);
+    return this.safeUrl;
   }
 
 }
