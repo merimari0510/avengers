be2de27caac0cf517a94133d2723515ca7796767
merimari0510 <merimari051094@gmail.com>
2020-01-07 23:35:47 +0100

    Crear la pagina de una MOVIE con la imagen central, el nombre, la fehca de estreno, la duracion, la sinopsis y el trailer

--- src/app/components/movie/movie.component.ts
+++ src/app/components/movie/movie.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service'
+import { ActivatedRoute } from '@angular/router'
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-movie',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
-export class MovieComponent implements OnInit {
+export class MovieComponent {
 
-  constructor() { }
+  movie:any = {};
+  safeUrl:any;
 
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute,
+              private _moviesService:MoviesService,
+              private _sanitizer: DomSanitizer) {
+    this.activatedRoute.params.subscribe( params => {
+      this.movie = this._moviesService.getMovie(params['id']);
+    })
   }
+
+  getTrailer() {
+    console.log(this.movie.trailer);
+    let video = this.movie.trailer;
+    this.safeUrl = '';
+    console.log(this.safeUrl);
+    if (video != '')
+      this.safeUrl = this._sanitizer.bypassSecurityTrustResourceUrl(video);
+    console.log(this.safeUrl);
+    return this.safeUrl;
+  }
+
+  getFormatDateMovie(date:Date) {
+    let month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+
+    let fullMonth = month[date.getMonth()];
+    let fullYear = date.getFullYear();
+
+    return date.getDay() + " " + fullMonth + " " + fullYear;
+  }
+
 
 }
